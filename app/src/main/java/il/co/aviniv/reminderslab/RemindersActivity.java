7b85fe9d95647ad456926965466d473a6f531931
Nofar Niv <avi.niv.2@gmail.com>
2015-08-20 14:15:17 +0300

    .

--- app/src/main/java/il/co/aviniv/reminderslab/RemindersActivity.java
+++ app/src/main/java/il/co/aviniv/reminderslab/RemindersActivity.java
@@ -1,15 +1,20 @@
 package il.co.aviniv.reminderslab;
 
+import android.annotation.TargetApi;
 import android.app.Dialog;
 import android.database.Cursor;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -102,10 +107,57 @@ public class RemindersActivity extends AppCompatActivity {
                     Toast.makeText(RemindersActivity.this, "clicked " + position, Toast.LENGTH_SHORT).show();
                 }*/
             });
+
+
+            //check version for new features compitability
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+                mListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+
+                    @Override
+                    public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) { }
+
+                    @TargetApi(Build.VERSION_CODES.HONEYCOMB)   //added by the android studio
+                    @Override
+                    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                        MenuInflater inflater = mode.getMenuInflater();
+                        inflater.inflate(R.menu.cam_menu, menu);
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                        switch(item.getItemId()) {
+                            case R.id.menu_item_delete_reminder:
+                                for (int nC = mCursorAdapter.getCount() - 1; nC >= 0; nC--) {
+                                    if (mListView.isItemChecked(nC)) {
+                                        mDbAdapter.deleteReminderById(getIdFromPosition(nC));
+                                    }
+                                }
+                                mode.finish();
+                                mCursorAdapter.changeCursor(mDbAdapter.fetchAllReminders());
+                                return true;
+                        }
+                        return false;
+                    }
+
+                    @Override
+                    public void onDestroyActionMode(ActionMode mode) { }
+                });
+            }
         }
         catch (Exception ex) {
             Utilities.showException(this, ex);
         }
+    }
+
+    private int getIdFromPosition(int nC) {
+        return (int)mCursorAdapter.getItemId(nC);
     }
 
     @Override
